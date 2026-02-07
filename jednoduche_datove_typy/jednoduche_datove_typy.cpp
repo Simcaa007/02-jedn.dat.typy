a97cbe22e8dcd56f71e390242f327b5f6450837c
Simcaa007 <havelkova.si@seznam.cz>
2026-02-07 14:05:46 +0100

    hotovo

--- jednoduche_datove_typy/jednoduche_datove_typy.cpp
+++ jednoduche_datove_typy/jednoduche_datove_typy.cpp
@@ -3,7 +3,39 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	cout << "Zadejte cele cislo n:";
+	cin >> n;
+	int soucetAscii = 0;
+	int soucetCifer = 0;
+	bool vetsiSoucet = false;
+
+	for (int i = 0; i < n && !vetsiSoucet; i++) {
+		cout << "Zadejte " << i + 1 << ". znak: " << endl;
+		char ch;
+		cin >> ch;
+
+		soucetAscii += ch;
+
+		if (ch > '0' && ch <= '9') {
+			soucetCifer += ch - '0';
+		}
+
+		if (soucetAscii > 1000) {
+			vetsiSoucet = true;
+		}
+	}
+
+	cout << "Soucet cifer je: " << soucetCifer << endl;
+
+	if (vetsiSoucet) {
+		cout << "Soucet ASCII kodu je vetsi jak 1000" << endl;
+	}
+	else {
+		cout << "Soucet ASCII kodu neni vetsi jak 1000" << endl;
+	}
 }
