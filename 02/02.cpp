13065c2ccc9acba3c29d25b7a832b0fcd7a8033b
Simcaa007 <havelkova.si@seznam.cz>
2026-02-07 14:16:32 +0100

    hotovo

---
+++ 02/02.cpp
@@ -0,0 +1,39 @@
+﻿// 02.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char znak;
+	cout << "Zadejte znaky za sebou dokud nezadate *" << endl;
+	bool alfa = false;
+	int soucetalfa = 0;
+
+
+	do {
+		cin >> znak;
+
+		if (znak >= 'a' && znak <= 'z') {
+			cout << "(" << (char)(znak - 32) << ")" << endl;
+		}
+		else if (znak >= 'A' && znak <= 'Z') {
+			cout << "(" << (char)(znak + 32) << ")" << endl;
+		}
+
+		if (znak >= 'a' && znak <= 'z' || znak >= 'A' && znak <= 'Z' || znak >= '0' && znak <= '9') {
+			soucetalfa++;
+		}
+
+		if (soucetalfa >= 5) {
+			alfa = true;
+		}
+
+	} while (znak != '*' && !alfa);
+
+	if (alfa) {
+		cout << "Zadali jste 5 alfanumerickych znaku" << endl;
+	}
+}
