d1d7e4407129291a0a3b3e2362a6598bd747910b
Simcaa007 <havelkova.si@seznam.cz>
2026-02-07 15:47:28 +0100

    hotovo

---
+++ 04/04.cpp
@@ -0,0 +1,31 @@
+﻿// 04.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    char znak;
+	bool licha = false;
+	cout << "Zadavejte znaky a program se ukonci az zadate *" << endl;
+
+    do {
+		cin >> znak;
+		if (znak >= '0' && znak <= '9' && znak % 2 != 0) {
+			licha = true;
+		}
+		cout << "(" << (int)znak << ", " << (char)((int)znak -1) << ", " << (char)((int)znak + 1) << ")" << endl;
+
+	} while (znak != '*' && !licha);
+    
+	if (licha) {
+		cout << "Bylo zadano liche cislo" << endl;
+	}
+	else {
+		cout << "Nebylo zadano zadne liche cislo" << endl;
+	}
+
+}
