cd724362901fcba79ad458dc856b36a55b6c1a58
Simcaa007 <havelkova.si@seznam.cz>
2026-02-08 19:24:26 +0100

    hotovo

---
+++ 09/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int minSude = 0;
+            bool sudeNalezeno = false;
+            int i = 0;
+            int radky = textBox1.Lines.Length;
+
+            while (i < radky && !sudeNalezeno)
+            {
+                int cislo = int.Parse(textBox1.Lines[i]);
+                if (cislo % 2 == 0)
+                {
+                    minSude = cislo;
+                    sudeNalezeno = true;
+                }
+                i++;
+            }
+
+            while (i < radky)
+            {
+                int cislo = int.Parse(textBox1.Lines[i]);
+                if (cislo % 2 == 0 && cislo < minSude)
+                {
+                    minSude = cislo;
+                }
+                i++;
+            }
+
+            if (!sudeNalezeno)
+            {
+                MessageBox.Show("Nenalezeno zadne sude cislo.");
+                return;
+            }
+            else
+            {
+                MessageBox.Show($"Nejmensi sude cislo je {minSude}.");
+            }
+        }
+    }
+}
