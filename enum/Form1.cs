561c090e778b57b8e1444a8ed6172e4cfbf73556
Simcaa007 <havelkovas.07@spst.eu>
2026-02-10 11:36:06 +0100

    hotovo

---
+++ enum/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @enum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum dny
+        {
+            Pondeli,
+            Utery,
+            Streda¨,
+            Ctvrtek,
+            Patek,
+            Sobota,
+            Nedele
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Prvni den v tydnu je: {dny.Pondeli}");
+        }
+    }
+}
